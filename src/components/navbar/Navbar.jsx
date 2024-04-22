0059a80bb7bf69ea689c87e392f8256c812a84b3
jatingarg0410 <gargjatin0410@gmail.com>
2024-04-23 00:44:24 +0530

    created responsive navbar design

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './navbar.scss'
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <a className="logo">
+          <img src="/logo.png" alt="" />
+          <span>GargEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contacts</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
